8d5f3c649b65d60cd5c1521fe6446ab150f3ab4f
Eneko Bikandi <ebikandi001@gmail.com>
2018-02-09 20:22:01 +0100

    first version of the QueueManager

---
+++ src/Queue/QueueManager.ts
@@ -0,0 +1,26 @@
+import { EventEmitter } from 'events';
+
+export default class QueueManager<T> extends EventEmitter {
+  private length: number;
+  private queue: T[];
+
+  constructor() {
+    super();
+    this.queue = [];
+    this.length = 0;
+  }
+  public enqueue(item: T) {
+    this.length = this.queue.push(item);
+    this.emit('item-queued');
+  }
+
+  public getFirstItem(): T {
+    const item = this.queue.shift() as T;
+    this.length--;
+    return item;
+  }
+
+  public itemCount(): number {
+    return this.length;
+  }
+}
