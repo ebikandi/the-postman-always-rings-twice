ff20fb818dad8d28ce3e203e8058b2345339a73f
Eneko Bikandi <eneko.bikandi@ve.com>
2018-02-09 13:51:09 +0100

    parcel tests

--- src/Parcel/Parcel.spec.ts
+++ src/Parcel/Parcel.spec.ts
@@ -1,7 +1,51 @@
+import { EventEmitter } from 'events';
 import Parcel from './Parcel';
 
 describe('Parcel', () => {
-  it('should run the test', () => {
-    expect(true).toBeTruthy();
+  let parcel: Parcel;
+
+  describe('sending the parcel', () => {
+    it('should succeed with a 100% success rate', done => {
+      parcel = new Parcel();
+      parcel.on('successfully-sent', () => {
+        expect(true).toBeTruthy();
+        done();
+      });
+      parcel.send(1);
+    });
+
+    it('should fail with a 0% success rate', done => {
+      parcel = new Parcel(0);
+      parcel.on('dead-inbox', () => {
+        expect(true).toBeTruthy();
+        done();
+      });
+      parcel.send(0);
+    });
+
+    it('should retry with a 0% success rate', done => {
+      parcel = new Parcel(1);
+      parcel.on('parcel-ready', () => {
+        expect(true).toBeTruthy();
+        done();
+      });
+      parcel.send(0);
+    });
+
+    it('should retry with a 0% success rate and then fail', done => {
+      let retried = false;
+      parcel = new Parcel(1);
+
+      parcel.on('parcel-ready', () => {
+        retried = true;
+        parcel.send(0);
+      });
+
+      parcel.on('dead-inbox', () => {
+        expect(retried).toBeTruthy();
+        done();
+      });
+      parcel.send(0);
+    });
   });
 });
--- src/Parcel/Parcel.ts
+++ src/Parcel/Parcel.ts
@@ -2,27 +2,30 @@ import { EventEmitter } from 'events';
 
 // Time until to do the retry in ms
 const DELAYS = [0, 100, 300, 1000];
-const MAX_RETRIES = 3;
 
 export default class Parcel extends EventEmitter {
   private retries: number;
+  private maxRetries: number;
 
-  constructor() {
+  constructor(maxRetries: number = 3) {
     super();
     this.retries = 0;
+    this.maxRetries = maxRetries >= 0 && maxRetries <= 3 ? maxRetries : 3;
   }
 
   public send(succesRate: number) {
-    const successfullySent = Math.random() <= succesRate;
+    const successfullySent = Math.random() < succesRate;
+
     if (successfullySent) {
       this.emit('successfully-sent');
     } else {
       this.retries++;
-      this.retries <= MAX_RETRIES
-        ? setTimeout(
-            () => this.emit('parcel-ready', this),
-            DELAYS[this.retries]
-          )
+
+      console.log(`condition: ${this.retries <= this.maxRetries}`);
+      this.retries <= this.maxRetries
+        ? setTimeout(() => {
+            this.emit('parcel-ready');
+          }, DELAYS[this.retries])
         : this.emit('dead-inbox');
     }
   }
