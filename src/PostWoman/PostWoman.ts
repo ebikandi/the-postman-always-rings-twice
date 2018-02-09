3916add01b5326868a3539295605f290065dcb05
Eneko Bikandi <ebikandi001@gmail.com>
2018-02-09 20:36:59 +0100

    continue with the PostWoman

--- src/PostWoman/PostWoman.ts
+++ src/PostWoman/PostWoman.ts
@@ -1,9 +1,13 @@
+import Parcel from '../Parcel/Parcel';
+import QueueManager from '../Queue/QueueManager';
+
 export default class PostWoman {
   private inbox: any;
-  private premiumRetryQueue: any[];
-  private premiumNewQueue: any[];
-  private regularRetryQueue: any[];
-  private regularNewQueue: any[];
+  // TODO study if all this queues can be somewhere else
+  private premiumRetryQueue: QueueManager<Parcel>;
+  private premiumNewQueue: QueueManager<Parcel>;
+  private regularRetryQueue: QueueManager<Parcel>;
+  private regularNewQueue: QueueManager<Parcel>;
   private isBusy: boolean;
   private successRate: number;
 
@@ -12,14 +16,33 @@ export default class PostWoman {
     setInterval(() => this.setSuccessRate(), 1000);
   }
 
-  // public receivePackage() {}
+  public receivePackage(code: string, employee: string, premium: boolean) {
+    if (this.queuesEmpty() && this.postWomanAvailable()) {
+      // Parcel.send
+    } else {
+      // Enqueue parcel
+    }
+  }
+
+  private queuesEmpty(): boolean {
+    return (
+      this.premiumRetryQueue.isEmpty() &&
+      this.premiumNewQueue.isEmpty() &&
+      this.regularRetryQueue.isEmpty() &&
+      this.regularNewQueue.isEmpty()
+    );
+  }
+
+  private postWomanAvailable(): boolean {
+    return this.successRate >= 0.85;
+  }
 
   private initializeProps() {
     this.inbox = {};
-    this.premiumRetryQueue = [];
-    this.premiumNewQueue = [];
-    this.regularRetryQueue = [];
-    this.regularNewQueue = [];
+    this.premiumRetryQueue = new QueueManager<Parcel>();
+    this.premiumNewQueue = new QueueManager<Parcel>();
+    this.regularRetryQueue = new QueueManager<Parcel>();
+    this.regularNewQueue = new QueueManager<Parcel>();
     this.isBusy = false;
     this.setSuccessRate();
   }
