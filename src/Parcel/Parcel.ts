13dff5e43dd6e06ab52dbf52d21da88d60c1be2d
Eneko Bikandi <eneko.bikandi@ve.com>
2018-02-09 12:08:03 +0100

    Parcel emitter class

---
+++ src/Parcel/Parcel.ts
@@ -0,0 +1,29 @@
+import { EventEmitter } from 'events';
+
+// Time until to do the retry in ms
+const DELAYS = [0, 100, 300, 1000];
+const MAX_RETRIES = 3;
+
+class Parcel extends EventEmitter {
+  private retries: number;
+
+  constructor() {
+    super();
+    this.retries = 0;
+  }
+
+  private send(succesRate: number) {
+    const successfullySent = Math.random() <= succesRate;
+    if (successfullySent) {
+      this.emit('successfully-sent');
+    } else {
+      this.retries++;
+      this.retries <= MAX_RETRIES
+        ? setTimeout(
+            () => this.emit('parcel-ready', this),
+            DELAYS[this.retries]
+          )
+        : this.emit('dead-inbox');
+    }
+  }
+}
