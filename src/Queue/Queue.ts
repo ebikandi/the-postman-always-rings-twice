70c0bc7c4295e74dc5b31a5e70250d71b827879f
Eneko Bikandi <ebikandi001@gmail.com>
2018-02-10 16:32:31 +0100

    generic eventemitter queues to enqueue anything you want

---
+++ src/Queue/Queue.ts
@@ -0,0 +1,30 @@
+import { EventEmitter } from 'events';
+
+export default class Queue<T> extends EventEmitter {
+  private length: number;
+  private queue: T[];
+
+  constructor() {
+    super();
+    this.queue = [];
+    this.length = 0;
+  }
+  public enqueue(item: T) {
+    this.length = this.queue.push(item);
+    this.emit('item-queued');
+  }
+
+  public getFirstItem(): T {
+    const item = this.queue.shift() as T;
+    this.length--;
+    return item;
+  }
+
+  public itemCount(): number {
+    return this.length;
+  }
+
+  public isEmpty(): boolean {
+    return this.length === 0;
+  }
+}
