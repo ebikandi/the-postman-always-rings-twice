2fea678709b671d8480501d4162e9b0003b37392
Eneko Bikandi <ebikandi001@gmail.com>
2018-02-11 16:47:21 +0100

    [PostWoman]: sendOrQueue method and check if is alredy processing a parcel just to process them one at a time

--- src/PostWoman/PostWoman.ts
+++ src/PostWoman/PostWoman.ts
@@ -2,18 +2,15 @@ import { EventEmitter } from 'events';
 import Parcel from '../Parcel/Parcel';
 import ParcelEvents from '../Parcel/ParcelEvents';
 import ParcelQueueManager from '../Queue/ParcelQueueManager';
+import PostWomanEvents from './PostWomanEvents';
 
 const MIN_AVAILABILITY_RATE = 0.85;
 
-const POSTWOMAN_READY_EVENT = 'postwoman-ready';
-
-const getSuccessRate = () => 1 - (Math.random() * (0.21 - 0.05) + 0.05);
+const getSuccessRate = () => 1.0 - (Math.random() * (0.21 - 0.05) + 0.05);
 
 let successRate: number = getSuccessRate();
 
-const postWomanAvailable = () => successRate >= 0.85;
-
-let initialized: boolean = false;
+const postWomanAvailable = () => successRate >= MIN_AVAILABILITY_RATE;
 
 const parcelQueues = new ParcelQueueManager();
 
@@ -22,23 +19,27 @@ const emitter = new EventEmitter();
 const initializeSuccessRateCheck = () => {
   setInterval(() => {
     successRate = getSuccessRate();
-    if (successRate >= MIN_AVAILABILITY_RATE) {
-      emitter.emit(POSTWOMAN_READY_EVENT);
+    if (postWomanAvailable()) {
+      emitter.emit(PostWomanEvents.AVAILABLE);
+    } else {
+      emitter.removeListener(PostWomanEvents.AVAILABLE, processNextParcel);
+      emitter.once(PostWomanEvents.AVAILABLE, processNextParcel);
+      console.error(
+        `${new Date().toISOString()} [CRITICAL] postwoman not available`
+      );
     }
   }, 1000);
 };
 
-const processNextParcel = () => {
-  if (successRate >= MIN_AVAILABILITY_RATE) {
+let isProcessing = false;
+
+const processNextParcel = async () => {
+  if (postWomanAvailable() && isProcessing === false) {
     const parcel = parcelQueues.getNextParcel();
     if (!!parcel) {
-      parcel.send(successRate);
+      isProcessing = true;
+      await parcel.send(successRate);
     }
-  } else {
-    emitter.once(POSTWOMAN_READY_EVENT, processNextParcel);
-    console.error(
-      `${new Date().toISOString()} [CRITICAL] postwoman not available`
-    );
   }
 };
 
@@ -48,32 +49,45 @@ const subscribeToParcelEvents = () => {
       console.info(
         `${new Date().toISOString()} [INFO] Parcel ${p.getCode()} successfully delivered to ${p.getEmployee()}. Retries: ${p.getRetries()}`
       );
+      isProcessing = false;
       processNextParcel();
     })
     .on(ParcelEvents.RETRY, (p: Parcel) => {
       console.warn(
         `${new Date().toISOString()} [WARN] Parcel ${p.getCode()} failed to be delivered to to ${p.getEmployee()}. Retries: ${p.getRetries()}`
       );
+      isProcessing = false;
+      processNextParcel();
     })
     .on(ParcelEvents.DEAD, (p: Parcel) => {
       console.error(
         `${new Date().toISOString()} [ERROR] Parcel ${p.getCode()} won't be delivered to ${p.getEmployee()}. Retries: ${p.getRetries()}`
       );
+      isProcessing = false;
       processNextParcel();
     })
-    .on(ParcelEvents.READY, (p: Parcel) => {
-      parcelQueues.enqueue(p);
-      processNextParcel();
-    });
+    .on(ParcelEvents.READY, (p: Parcel) => sendOrQueue(p));
 };
 
-const initializeIfNeeded = () => {
-  if (!initialized) {
-    initialized = true;
-    initializeSuccessRateCheck();
-    subscribeToParcelEvents();
+const sendOrQueue = async (parcel: Parcel) => {
+  if (postWomanAvailable() === false || isProcessing === true) {
+    parcelQueues.queue(parcel);
+  } else if (isProcessing === false && parcelQueues.queuesAreEmpty()) {
+    isProcessing = true;
+    await parcel.send(successRate);
+  } else {
+    parcelQueues.queue(parcel);
+    processNextParcel();
   }
 };
+
+const initialize = () => {
+  initializeSuccessRateCheck();
+  subscribeToParcelEvents();
+  console.log('Initialized PostWoman');
+};
+
+initialize();
 
 const getParcelFromCarrier = (
   code: string,
@@ -81,9 +95,7 @@ const getParcelFromCarrier = (
   premium: boolean
 ) => {
   const parcel = new Parcel(code, employee, premium, emitter);
-  initializeIfNeeded();
-  parcelQueues.enqueue(parcel);
-  processNextParcel();
+  sendOrQueue(parcel);
 };
 
 export default { getParcelFromCarrier };
