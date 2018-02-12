1aab4fd9fb0c5b92b6fcb7a4ba30af0ba7abceab
Eneko Bikandi <ebikandi001@gmail.com>
2018-02-12 16:10:53 +0100

    fix bugy simulation and comment the code

--- src/StandardInputCarrier/StandardInputCarrier.ts
+++ src/StandardInputCarrier/StandardInputCarrier.ts
@@ -1,11 +1,20 @@
+/**
+ * A carrier who gets data from the standard input. Also, it performs a simulation if told so.
+ */
 import PostWoman from '../PostWoman/PostWoman';
 
 const readline = require('readline');
 
+/**
+ * It generates a regular or premium parcel and will give it to the PostWoman.
+ *
+ * @param verboseMode Flag to check if it has to print the generated parcel data
+ * @param id The Id to compone the parcel-code
+ */
 const generateRandomParcel = (verboseMode: boolean, id: number) => {
   const code = `SHIP${id}`;
   const employee = `employee${id}`;
-  const premium = Math.random() <= 0.35;
+  const premium = Math.random() <= 0.35; // The rate of the premium parcels will be %35
   const premiumToString = premium ? 'Premium' : 'Regular';
   if (verboseMode === true) {
     console.log(`${code},${employee}, ${premiumToString}`);
@@ -18,12 +27,22 @@ let stopper: any = null;
 const getSimMsg = (isVerBose: boolean) =>
   isVerBose ? '** starting sim -verbose **' : '** starting sim **';
 
+/**
+ * Starts a simulation setting a 1s timeout to trigger a parcel generation. It will call itself afer
+ * that second to set another timeout to crate another parcel, and so on.
+ * It saves the listener to remove it whennever we want and stop setting timeouts
+ * (and consequently stop creating parcels).
+ *
+ * If is another simulation running, it does not create it and prompts a message.
+ *
+ * @param verboseMode Flag to check if it has to print the generated parcel data
+ * @param id The Id to compone the parcel-code
+ */
 const simulation = (isVerbose = false, id = 0) => {
   if (stopper === null) {
-    const msg = getSimMsg(isVerbose);
-    console.log(msg);
     stopper = setTimeout(() => {
       id++;
+      stopper = null;
       generateRandomParcel(isVerbose, id);
       simulation(isVerbose, id);
     }, 1000);
@@ -32,20 +51,32 @@ const simulation = (isVerbose = false, id = 0) => {
   }
 };
 
+/**
+ * Creates the object to read from the standard input.
+ */
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false,
 });
+
+/**
+ * Function to initialize the carrier and listen to the stdin.
+ */
 export default () => {
   console.log('****************************');
   console.log('** STANDARD INPUT CARRIER **');
   console.log('****************************');
 
   rl.on('line', (line: string) => {
+    let msg = '';
     if (line === 'sim') {
+      msg = getSimMsg(false);
+      console.log(msg);
       simulation(false);
     } else if (line === 'sim --v') {
+      msg = getSimMsg(true);
+      console.log(msg);
       simulation(true);
     } else if (line === 'stop') {
       console.log('** stoping sim **');
@@ -55,7 +86,9 @@ export default () => {
       console.log('** exiting **');
       process.exit();
     } else {
-      // TODO check the line with regex
+      /**
+       * @todo check the line with regex
+       */
       const [code, employee, premium] = line.split(',');
       PostWoman.getParcelFromCarrier(
         code,
